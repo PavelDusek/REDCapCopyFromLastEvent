38d1681f3c87905032916de0897daa7a99a4ebfd
PavelDusek <pavel.dusek@gmail.com>
2016-10-30 23:17:47 +0100

    Starting the project.

---
+++ README.md
@@ -0,0 +1,7 @@
+# REDCapCopyFromLastEvent
+A hook for REDCap (https://projectredcap.org/) that enables you to use @COPYFROMEVENT action tag. It adds a button that enables you to copy value of a parameter from a different event.
+
+# Installation
+If you are not using any other hook, put hooks.php file to the redcap root directory on the web server and set the path to it in the Control Center -> General Configuration -> Other system settings -> REDCap Hooks.
+
+If you are using other hooks, rename the file hooks.php and make sure the function redcap_data_entry_form_top() is defined and this file is included in its body.
---
+++ hooks.php
@@ -0,0 +1,65 @@
+<?php
+
+function isInstrumentUsedInEvent( $instrument, $event_id ) {
+	$sql = sprintf( 
+		"SELECT `form_name` FROM `redcap_events_forms` WHERE `event_id`='%s';",
+		db_real_escape_string($event_id)
+	);
+	$q = db_query( $sql );
+	while ( $row = db_fetch_assoc($q) ) {
+		if( $instrument == $row['form_name'] ) return true;
+	}
+	return false;
+}
+
+function copy_from_last_event() {
+	$sql = sprintf(
+		"SELECT `field_name`,`misc` FROM `redcap_metadata` WHERE `project_id`='%s' AND `form_name`='%s';",
+		db_real_escape_string($project_id),
+		db_real_escape_string($instrument)
+	);
+	$q = db_query($sql);
+	while( $row = db_fetch_assoc($q) ) {
+		if ( !is_null($row['misc']) )
+			//TODO regex @GETDATAFROMLASTEVENT
+			echo "<p>" . $row['field_name'] . ":" . $row['misc'] . "</p>";
+	}
+
+	$events = REDCap::getEventNames(true);
+	$previous_event_id = 0;
+	foreach ( array_keys($events) as $id ) {
+		if ($id == $event_id)
+			break;
+		else {
+			if ( isInstrumentUsedInEvent($instrument,$id) )
+				$previous_event_id = $id;
+		}
+	}
+	echo "<p> this event id: $event_id</p>";
+	echo "<p> previous event id: $previous_event_id </p>";
+?>
+<script type='text/javascript'>
+	function getDataFromLastEvent () {
+		alert('project_id: ' + '<?php echo $project_id;?>');
+		alert('record: ' + '<?php echo $record;?>');
+		alert('instrument: ' + '<?php echo $project_id;?>');
+		alert('event_id: ' + '<?php echo $event_id;?>');
+		alert('group_id: ' + '<?php echo $group_id;?>');
+<?php
+	$events = REDCap::getEventNames(true);
+	foreach (array_keys($events) as $event) {
+		print "alert('event: $event');";
+	}
+?>
+	}
+</script>
+<button onClick='getDataFromLastEvent();'>Copy from last event</button>
+
+<?php
+}
+
+
+function redcap_data_entry_form_top($project_id, $record, $instrument, $event_id, $group_id) {
+	if ( REDCap::isLongitudinal() ) copy_from_last_event();
+}
+?>
