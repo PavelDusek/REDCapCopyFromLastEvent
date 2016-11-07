728ca1276b13d9566922364100a0c8e9426ff8a7
PavelDusek <pavel.dusek@gmail.com>
2016-11-07 11:40:14 +0100

    debugging flag

--- hooks.php
+++ hooks.php
@@ -1,11 +1,17 @@
 <?php
+$debugging = true;
+
 function get_fields_with_action_tag( $project_id, $instrument, $action_tag_regex ) {
 	//Search for all fields in given instrument in given project.
+	global $debugging;
+	$debugging = true;
+
 	$sql = sprintf(
 		"SELECT `field_name`,`misc` FROM `redcap_metadata` WHERE `project_id`='%s' AND `form_name`='%s';",
 		db_real_escape_string($project_id),
 		db_real_escape_string($instrument)
 	);
+	if ($debugging) echo "\$sql: $sql<br />\n";
 	$q = db_query($sql);
 
 	//Walk through each question and look if it matches given action tag regex.
@@ -19,10 +25,14 @@ function get_fields_with_action_tag( $project_id, $instrument, $action_tag_regex
 
 function isInstrumentUsedInEvent( $instrument, $event_id ) {
 	//Search for all instruments in given event.
+	global $debugging;
+	$debugging = true;
+
 	$sql = sprintf( 
 		"SELECT `form_name` FROM `redcap_events_forms` WHERE `event_id`='%s';",
 		db_real_escape_string($event_id)
 	);
+	if ($debugging) echo "\$sql: $sql<br />\n";
 	$q = db_query( $sql );
 	//Walk through each instrument and test if it is the given one. If it is, then return true.
 	while ( $row = db_fetch_assoc($q) ) {
@@ -33,7 +43,12 @@ function isInstrumentUsedInEvent( $instrument, $event_id ) {
 
 function copy_from_last_event($project_id, $record, $instrument, $event_id, $group_id) {
 	//Get all fields in this project and this instrument with the action tag @COPYFROMLASTEVENT.
+	global $debugging;
+	$debugging = true;
+	echo "debugging: $debugging<br/>\n";
+
 	$has_given_action_tag = get_fields_with_action_tag( $project_id, $instrument, '/@COPYFROMLASTEVENT/' );
+	if ($debugging) { echo "\$has_given_action_tag:"; print_r( $has_given_action_tag ); echo "<br />\n"; }
 
 	//Search for the last event that this isnstrument is used in.
 	$events = REDCap::getEventNames(false, false);
@@ -46,6 +61,7 @@ function copy_from_last_event($project_id, $record, $instrument, $event_id, $gro
 			$previous_event_name = $events[$id];
 		}
 	}
+	if ($debugging) echo "\$previous_event_id: $previous_event_id, \$previous_event_name: $previous_event_name<br/>\n";
 
 	//Run only if there is a previous event and if there are any fields with the action tag @COPYFROMLASTEVENT
 	if ($previous_event_id && (!empty($has_given_action_tag)) ) {
@@ -54,6 +70,8 @@ function copy_from_last_event($project_id, $record, $instrument, $event_id, $gro
 		$data = REDCap::getData( $project_id, 'array', $record, $has_given_action_tag, $previous_event_id );
 		$data_for_this_record = array_pop($data);
 		$data_for_this_record_and_event = array_pop($data_for_this_record);
+
+		if ($debugging) { echo "\$data_for_this_record_and_event:"; print_r( $data_for_this_record_and_event ); echo "<br /><br /><hr />\n"; }
 
 		//Create a button to run the code.
 		echo "<button id='copy-from-last-event-button' type='button' onClick='copyFromLastEvent();'>Copy data from event $previous_event_name</button>\n";
@@ -66,6 +84,8 @@ function copy_from_last_event($project_id, $record, $instrument, $event_id, $gro
 				echo "\t\t$('input[name=$field_name]').val('$field_value');\n";
 				//Code for radio input fields (it should not be a problem, if the radio does not exist):
 				echo "\t\t$('input:radio[name=" . $field_name . "___radio][value=$field_value]').click();\n";
+				//Code for checkbox input fields (it should not be a problem, if the checkbox does not exist):
+				echo "\t\t$('input:checkbox[name=__chkn__" . $field_name . "][code=$field_value]').click();\n";
 			}
 		}
 		echo "\t}\n\n";
